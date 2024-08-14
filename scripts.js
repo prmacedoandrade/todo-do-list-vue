cad8e6378ad5030f87a1d0ef53fe48db40da3f14
Paulo Renato Macêdo <prmacedoandrade@gmail.com>
2024-08-14 20:29:23 -0300

    Testing lifecycle hooks

--- scripts.js
+++ scripts.js
@@ -16,14 +16,29 @@ const TodoApp = {
                 this.newTask = {
                     isDone: false
                 };
+
+                localStorage.setItem("taks", JSON.stringify(this.tasks));
+
+
             }else{
                 alert("Todo text is required")
             }
 
         }
+    },
+    beforeCreate(){
+        console.log("BEFORE CREATE");
+    },
+    created(){
+        console.log("created");
+    },
+    beforeUpdate(){
+        console.log("BEFORE UPDATE");
     }
+
 
 
 };
 
 Vue.createApp(TodoApp).mount('#app');
+
